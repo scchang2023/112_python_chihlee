037499e016ddc42dd8563a45f656e5731dfd4be3
章士祺 <126841600+scchang2023@users.noreply.github.com>
2023-07-08 08:14:08 +0000

    modified

--- lesson6_2.py
+++ lesson6_2.py
@@ -1,30 +1,30 @@
 import random
 
-min = 1
-max = 100
-count = 0
-target = random.randint(min, max)
-print(f"target is  {target}")
-print("============= 猜數字遊戲 ==============")
+def playGame():
+    min = 1
+    max = 100
+    count = 0
+    target = random.randint(min, max)
+    print(f"target is  {target}")
+    print("============= 猜數字遊戲 ==============")
+
+    while True:
+        keyin = int(input(f"猜數字範圍{min}~{max}:"))
+        count += 1
+        if keyin == target:
+            print(f"賓果! 猜對了，答案是：{target}")
+            print(f"你共猜了{count}次")
+            break
+        elif keyin > target:
+            print("再小一點")
+            max = keyin - 1
+        else:
+            print("再大一點")
+            min = keyin + 1
 
 while True:
-    keyin = int(input(f"猜數字範圍{min}~{max}:"))
-    count += 1
-    if keyin == target:
-        print(f"賓果! 猜對了，答案是：{target}")
-        print(f"你共猜了{count}次")
-        if input("要繼續玩嗎？(y/n)") == 'n':
-            break
-        else:
-            min = 1
-            max = 100
-            count = 0
-            target = random.randint(min, max)
-            print(f"target is  {target}")
-    elif keyin > target:
-        print("再小一點")
-        max = keyin - 1
-    else:
-        print("再大一點")
-        min = keyin + 1
+    playGame()
+    play_again = input("還要玩嗎(y/n)")
+    if not play_again == 'y':
+        break
 print("遊戲結束")
